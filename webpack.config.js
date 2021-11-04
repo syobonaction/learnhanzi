3f2ea2d4d13132aa6d7b07292c7c52d2ea8b4ac4
Charles Cole <smashdaisaku@gmail.com>
2021-11-04 23:57:00 +0100

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+};
